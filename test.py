e383b436a0e2d8273948d729868c5027157b6fc8
snjsomnath <53001297+snjsomnath@users.noreply.github.com>
2020-04-26 13:38:04 +0200

    Added dashboard
    Dashboard and additional resources updated

--- test.py
+++ test.py
@@ -3,12 +3,18 @@ from PIL import Image
 import cv2
 import numpy as np
 
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-file_path= "resources/03.jpg"
+names_actual = []
+result_name = []
+result_wealth = []
+
+file_path= "resources/14.jpg"
 im = Image.open(file_path)
 im.save("ocr.jpg", dpi=(300, 300))
- 
- 
+
 #make image b&w
 im = Image.open("ocr.jpg").convert("L")
 im.save("ocr.jpg")
@@ -21,13 +27,14 @@ invert = cv2.bitwise_not(image)
 #Crop image to name boxes
 #Crop dimensions for names
 crop1 = invert[428:478,683:970]
-crop2 = invert[655:690,683:970]
+crop2 = invert[645:700,683:970]
 crop3 = invert[869:918,683:970]
 crop4 = invert[1086:1136,683:970]
 
 
 #Global Crop width for wealth except first value
-#ww1 = 2085
+#ww1 = 2067
+#ww2 = 2198
 ww1 = 2085
 ww2 = 2225
 #Global variables for threshold
@@ -65,17 +72,132 @@ name3 = pytesseract.image_to_string(threshold3)
 name4 = pytesseract.image_to_string(threshold4)
 
 custom_config = r'--oem 3 --psm 6 outputbase digits'
-wealth1 = pytesaseract.image_to_string(threshold1a, config=custom_config)
+wealth1 = pytesseract.image_to_string(threshold1a, config=custom_config)
 wealth2 = pytesseract.image_to_string(threshold2a, config=custom_config)
 wealth3 = pytesseract.image_to_string(threshold3a, config=custom_config)
 wealth4 = pytesseract.image_to_string(threshold4a, config=custom_config)
+
 wealth1 = int(wealth1)
 wealth2 = -abs(int(wealth2))
 wealth3 = -abs(int(wealth3))
 wealth4 = -abs(int(wealth4))
- 
+
+df = pd.read_csv('data.csv')
+
+result_name.append(name1,name2,name3,name4)
+result_wealth.append(wealth1,wealth2,wealth3,wealth4)
 
 print(name1,name2,name3,name4)
 print(wealth1,wealth2,wealth3,wealth4)
-#with open("Output.csv", "w",5 ,"utf-8") as text_file:
-    #text_file.write(name1+","+wealth1+"\n"+name2+","+wealth2+"\n"+name3+","+wealth3+"\n"+name4+","+wealth4)
+
+
+pd.DataFrame(columns=['thamarmo','Sanjay','Spartan','Abhishek','Shishtaouk','NipPincher'])
+         data = pd.read_csv('data.csv')
+         for rows in data:
+             
+
+
+#Append the latest scan to data.csv
+
+
+
+
+
+
+#Read CSV and plot the chart
+
+
+
+fig = make_subplots(
+    rows=6, cols=3,
+    specs=[[{}, {"rowspan": 2,"colspan": 2},None],
+        [{}, None,None],
+        [{}, None,None],
+        [{}, None,None],
+        [{}, None,None],
+        [{}, None,None]],
+    subplot_titles=("Sanjay","Rank tracking", "Thamar", "Abhishek", "Shishir","Nivin","Hrideek"))
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Sanjay_w'], name="Sanjay",line_shape='spline',connectgaps=True,showlegend=False),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Thamar_w'], name="Thamar",line_shape='spline',connectgaps=True,showlegend=False),
+    row=2, col=1
+)
+
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Abhishek_w'], name="Abhishek",line_shape='spline',connectgaps=True,showlegend=False),
+    row=3, col=1
+)
+
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Shishir_w'], name="Shishir",line_shape='spline',connectgaps=True,showlegend=False),
+    row=4, col=1
+)
+
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Nivin_w'], name="Nivin",line_shape='spline',connectgaps=True,showlegend=False),
+    row=5, col=1
+)
+
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Hrideek_w'], name="Hrideek",line_shape='spline',connectgaps=True,showlegend=False),
+    row=6, col=1
+)
+
+#Rank tracking
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Sanjay_r'], name='Sanjay',line_shape='spline',connectgaps=True),
+    row=1,col=2
+)
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Thamar_r'], name = 'Thamar',line_shape='spline',connectgaps=True),
+    row=1,col=2
+)
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Abhishek_r'], name='Abhishek',line_shape='spline',connectgaps=True),
+    row=1,col=2
+)
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Shishir_r'], name='Shishir',line_shape='spline',connectgaps=True),
+    row=1,col=2
+)
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Nivin_r'], name = 'Nivin',line_shape='spline',connectgaps=True),
+    row=1,col=2
+)
+fig.add_trace(
+    go.Scatter(
+    x=df['Game No'], 
+    y=df['Hrideek_r'], name='Hrideek',line_shape='spline',connectgaps=True),
+    row=1,col=2
+)
+
+
+fig.update_layout(height=800, width=1600, title_text="Monopoly Tracking Dashboard")
+fig.show()
+
